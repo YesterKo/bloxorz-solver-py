8ba27f43a4d15ae4e88a5aba68d0419203552a9d
Jesper Korsen <Jesper.Korsen@gmail.com>
2022-11-23 11:30:49 +0200

    natuke kirjuatmist

--- bloxorz-solver-py.py
+++ bloxorz-solver-py.py
@@ -16,25 +16,17 @@ kaart = (
 plokk = ((1,1),(1,1)) #0 alati vasakul üleval, 1 paremal all
 teada = {plokk:0}
 edasi = {plokk}
-auk = [10,10]
+auk = (10,10)
 kaigunr = 0
 
-
-while edasi:
-    kaigunr += 1
-    uued = []
-    for uuritav in edasi:
-        for kaik in ['N','S','E','W']:
-            uus = move(uuritav, kaik)
-            if 
-
 #0 - up, 1 - down, 2 - left, 3 - right
-def move(iplokk, suund):
+def move(i, suund):
+    iplokk = [[i[0][0],i[0][1]],[i[1][0],i[1][1]]]
     if iplokk[0] == iplokk[1]:
         if suund == "N":#match tuleb alles 3.10-s, mul ja mujal ka 3.9
             iplokk[0][1] -= 2
             iplokk[1][1] -= 1
-        elif if suund == "S":
+        elif suund == "S":
             iplokk[0][1] += 1
             iplokk[1][1] += 2
         elif suund == "E":
@@ -47,7 +39,7 @@ def move(iplokk, suund):
         if suund == "N":
             iplokk[0][1] -= 1
             iplokk[1][1] -= 1
-        elif if suund == "S":
+        elif suund == "S":
             iplokk[0][1] += 1
             iplokk[1][1] += 1
         elif suund == "E":
@@ -60,7 +52,7 @@ def move(iplokk, suund):
         if suund == "N":
             iplokk[0][1] -= 2
             iplokk[1] = iplokk[0]
-        elif if suund == "S":
+        elif suund == "S":
             iplokk[0][1] += 1
             iplokk[1] = iplokk[0]
         elif suund == "E":
@@ -69,18 +61,37 @@ def move(iplokk, suund):
         elif suund == "W":
             iplokk[0][0] -= 1
             iplokk[1][0] -= 1
-    if kaart[iplokk[0][0]] == 0 or kaart[iplokk[1][0]] == 0 or kaart[iplokk[0][1]] == 0 or kaart[iplokk[1][1]] == 0:
-        return [[-1,-1],[-1,-1]]#dead
-    return iplokk
+    return tuple(iplokk)
 
 #Dictionary positsioonidest ja ss käigud mis sellele eelnesid
 
-def nextMove(path,dir,blokk):
+# def nextMove(path,dir,blokk):
+# 
+#     if blokk[1]=blokk[0]=auk:
+#         return True, path
+#     else:
+#         return nextMove(path,"N",blokk)
+#         return nextMove(path,"S",blokk)
+#         return nextMove(path,"E",blokk)
+#         return nextMove(path,"W",blokk)
 
-    if blokk[1]=blokk[0]=auk:
-        return True, path
-    else:
-        return nextMove(path,"N",blokk)
-        return nextMove(path,"S",blokk)
-        return nextMove(path,"E",blokk)
-        return nextMove(path,"W",blokk)
+while edasi:
+    kaigunr += 1
+    uued = []
+    for uuritav in edasi:
+        for kaik in ['N','S','E','W']:
+            uus = move(uuritav, kaik)
+            if uus[0] == auk and uus[1] == auk:
+                win = 1 # kiire fix
+                break # kiire fix
+            if kaart[uus[0][0]][uus[0][1]] == 1 and kaart[uus[1][0]][uus[1][1]] == 1 and uus not in teada: 
+                uued.append(uus)
+                teada[uus] = uuritav
+        if win == 1: break # kiire fix
+    if win == 1: break # kiire fix
+    edasi = set()
+    
+koht = [auk,auk]
+while koht != plokk:
+    print(koht)
+    koht = teada[koht]
